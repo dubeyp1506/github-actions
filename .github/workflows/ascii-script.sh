e64f73f1324fce3d7449f4ab3d2b1efeef6ecefd
Priyanshu Dubey <dubeyp1506@gmail.com>
2026-02-15 10:04:41 +0530

    fix path namme

--- .github/workflows/ascii-script.sh
+++ .github/workflows/ascii-script.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 sudo apt-get install cowsay -y
-cowsay -f dragon "this is a dragon ascii artwork" >> dragon.txt
+/usr/games/cowsay -f dragon "this is a dragon ascii artwork" >> dragon.txt
 grep -i "dragon" dragon.txt
 cat dragon.txt
